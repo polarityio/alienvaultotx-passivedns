194f2d10caae030eb8cd89a71e3b35dd6c019156
CJ-Polarity <127783370+CJ-Polarity@users.noreply.github.com>
2023-11-27 10:34:07 -0500

    chore:fixes to config

--- config/config.js
+++ config/config.js
@@ -1,56 +1,56 @@
 module.exports = {
-  polarityIntegrationUuid: "7e3673c0-855b-11ee-a3cb-2d9498bdef4f",
-  name: "AlienvaultOTX - PassiveDNS",
-  acronym: "ADNS",
-  description: "Converts timestamps into any Time Zone",
-  defaultColor: "light-gray",
-  entityTypes: ["domain"],
-  styles: ["./styles/styles.less"],
+  polarityIntegrationUuid: '7e3673c0-855b-11ee-a3cb-2d9498bdef4f',
+  name: 'AlienvaultOTX - PassiveDNS',
+  acronym: 'ADNS',
+  description: 'Query Alienvault OTX using a domain name to get passive DNS information',
+  defaultColor: 'light-gray',
+  entityTypes: ['domain'],
+  styles: ['./styles/styles.less'],
   block: {
     component: {
-      file: "./components/block.js",
+      file: './components/block.js'
     },
     template: {
-      file: "./templates/block.hbs",
-    },
+      file: './templates/block.hbs'
+    }
   },
   request: {
-    cert: "",
-    key: "",
-    passphrase: "",
-    ca: "",
-    proxy: "",
+    cert: '',
+    key: '',
+    passphrase: '',
+    ca: '',
+    proxy: ''
   },
   logging: {
-    level: "info", //trace, debug, info, warn, error, fatal
+    level: 'info' //trace, debug, info, warn, error, fatal
   },
   options: [
     {
-      key: "apiKey",
-      name: "Alienvault OTX API Key",
-      description: "Alienvault OTX",
-      default: "",
-      type: "password",
+      key: 'apiKey',
+      name: 'Alienvault OTX API Key',
+      description: 'Alienvault OTX',
+      default: '',
+      type: 'password',
       userCanEdit: true,
-      adminOnly: false,
+      adminOnly: true
     },
     {
-      key: "dataFormat",
-      name: "Data Format",
-      description: "Data Format to return",
-      default: "y-LL-dd TT",
-      type: "text",
+      key: 'dateFormat',
+      name: 'Date Format',
+      description: 'Date Format to return',
+      default: 'y-LL-dd TT',
+      type: 'text',
       userCanEdit: true,
-      adminOnly: false,
+      adminOnly: true
     },
     {
-      key: "maxResults",
-      name: "Max Results",
-      description: "Max number of results to return",
-      default: 25,
-      type: "number",
+      key: 'maxResults',
+      name: 'Max Results',
+      description: 'Max number of results to return',
+      default: 50,
+      type: 'number',
       userCanEdit: true,
-      adminOnly: false,
-    },
-  ],
+      adminOnly: true
+    }
+  ]
 };
