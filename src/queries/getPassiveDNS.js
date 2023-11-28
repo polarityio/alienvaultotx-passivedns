0163cd82605401222a0d3e5c2dac942f59dfdd56
Ed <ed@polarity.io>
2023-11-27 22:54:12 -0500

    Move constant into file, format with prettier
    Since the constant is only being used in one place put it where its being used

--- src/queries/getPassiveDNS.js
+++ src/queries/getPassiveDNS.js
@@ -1,23 +1,25 @@
-const { requestsInParallel } = require("../request");
-const { map } = require("lodash/fp");
-const { ALIENTVAULT_OTX_BASE_URL } = require("../constants");
+const { requestsInParallel } = require('../request');
+const { map } = require('lodash/fp');
+
+const ALIENVAULT_OTX_BASE_URL =
+  'https://otx.alienvault.com/otxapi/indicators/domain/passive_dns/';
 
 const getPassiveDNS = async (entities, options) => {
   const dnsRequests = map(
     (entity) => ({
       entity,
-      url: ALIENTVAULT_OTX_BASE_URL + entity.value,
-      method: "GET",
-      options,
+      url: ALIENVAULT_OTX_BASE_URL + entity.value,
+      method: 'GET',
+      options
     }),
     entities
   );
 
-  const dnsResponses = await requestsInParallel(dnsRequests, "body");
+  const dnsResponses = await requestsInParallel(dnsRequests, 'body');
 
   const dnsResponsesWithResultsObject = map((response) => ({
     ...response,
-    result: JSON.parse(response.result),
+    result: JSON.parse(response.result)
   }))(dnsResponses);
 
   return dnsResponsesWithResultsObject;
